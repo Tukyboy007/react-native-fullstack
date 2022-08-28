eeb4a35ddee4099d1e4f738065c5bca1714965a6
FRoster <froster@Omen.Omen>
2022-08-28 18:44:14 +0800

    test

--- functions/app.js
+++ functions/app.js
@@ -22681,7 +22681,7 @@ router.get("/", (req, res) => {
     hello: "hi!"
   });
 });
-app.use(`/api`, router);
+app.use(`/.netlify/functions/api`, router);
 module.exports = app;
 module.exports.handler = serverless(app);
 
